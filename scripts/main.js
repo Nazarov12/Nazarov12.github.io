2033666624f0aee3d367fda27b65e73c66459d7c
Nazarov12 <billy20000064@gmail.com>
2023-01-18 21:51:46 +0400

    adding my files to my repositiry

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+//Image switcher code
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/metro.png') {
+      myImage.setAttribute ('src','images/metro2.png');
+    } else {
+      myImage.setAttribute ('src','images/metro.png');
+    }
+}
+var myButton=document.querySelector('button');
+var myHeading=document.querySelector('h1');
+
+function setUserName() {
+  var myName=prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent='Метро 2033- лучшая книга о постапокалипсисе,'+ myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName=localStorage.getItem('name');
+  myHeading.textContent='Метро 2033- лучшая книга о постапокалипсисе,' + storedName;
+
+}
+
+myButton.onclick=function() {
+  setUserName();
+
+}
